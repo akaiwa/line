a3674be229fb0d25c4b505b40dc1b4aa5628545a
akaiwa <yuma-akaiwa@exa-corp.co.jp>
2016-11-20 15:03:34 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,6 +6,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-こちらはLINE Messaging APIのデモサイトです。
+  <img src="https://github.com/akaiwa/line/blob/master/web/ss1.png" alt="SS1" width="320" height="568" border="0" />
+  <br>
+  <img src="https://github.com/akaiwa/line/blob/master/web/ss2.png" alt="SS1" width="320" height="568" border="0" />
+  <br>
+  <img src="https://github.com/akaiwa/line/blob/master/web/ss3.png" alt="SS1" width="320" height="568" border="0" />
+  <br>
 </body>
 </html>
