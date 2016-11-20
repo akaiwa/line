7872fc7363ea0c3e9d97d16d1a2b647bc0467600
akaiwa <yuma-akaiwa@exa-corp.co.jp>
2016-11-20 15:09:55 +0900

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,11 +6,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-  <img src="https://github.com/akaiwa/line/blob/master/web/ss1.png" alt="SS1" width="320" height="568" border="0" />
+  <img src="ss1.png" alt="SS1" width="320" height="568" border="0" />
   <br>
-  <img src="https://github.com/akaiwa/line/blob/master/web/ss2.png" alt="SS1" width="320" height="568" border="0" />
+  <img src="ss2.png" alt="SS1" width="320" height="568" border="0" />
   <br>
-  <img src="https://github.com/akaiwa/line/blob/master/web/ss3.png" alt="SS1" width="320" height="568" border="0" />
+  <img src="ss3.png" alt="SS1" width="320" height="568" border="0" />
   <br>
 </body>
 </html>
