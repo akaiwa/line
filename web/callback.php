5f6aec0de4d03bf7f5cb8a8364a41c84d5cbcbfe
akaiwa <yuma-akaiwa@exa-corp.co.jp>
2016-11-30 16:48:27 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -376,7 +376,7 @@ if($type != "text"){
 		"text" => "20日14時、新宿のカフェAだね！\nお悩み解決できるよう僕もがんばるよ！"
 	];
 } else {	
-	$nlcurl = "https://watson-api-explorer.mybluemix.net/natural-language-classifier/api/v1/classifiers/8d6ce2x125-nlc-3260/classify";
+	$nlcurl = "https://watson-api-explorer.mybluemix.net/natural-language-classifier/api/v1/classifiers/b238f1x131-nlc-1159/classify";
 	$nlcch = curl_init($nlcurl);
 	$USERNAME = "02248a63-76ff-4dc8-92b5-a80944b077ed";
 	$PASSWORD = "2nEYStPrVjHB";
